ff9472430bf946f82eaf83a94d89697023125c19
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-17 18:51:23 +0000

    Finish Going in Circles

---
+++ RadiusOfCircle.java
@@ -0,0 +1,15 @@
+//Author: Jacob Liddicoat
+//Date created: Mar 17,2018
+import java.io.*;
+import java.util.*;
+public class RadiusOfCircle
+{
+ public static void main(String arg[])
+ {
+  Scanner kbReader = new Scanner(System.in);
+  System.out.print("61017.6104");
+  double d = kbReader.nextDouble();
+  System.out.println(Radius of your circle is: double sqrt( d / double PI)); // Radius of your circle is: 139.4
+ }
+}
+
